36ce6693c48350590f287b011636d412d71d815b
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-05-10 10:58:51 +0200

    Add images & local data

---
+++ src/localData/productsDataArray.js
@@ -0,0 +1,159 @@
+import productImg1 from '../assets/images/a-pocket-watch-laying-on-a-black-and-orange-background.jpg';
+import productImg2 from '../assets/images/black-white-wrist-watches.jpg';
+import productImg3 from '../assets/images/grooms-prep-kit-for-wedding.jpg';
+import productImg4 from '../assets/images/hanging-pocket-watch.jpg';
+import productImg5 from '../assets/images/mens-watch-in-sun.jpg';
+import productImg6 from '../assets/images/pocket-watch-on-black.jpg';
+import productImg7 from '../assets/images/round-marble-watches.jpg';
+import productImg8 from '../assets/images/time-3091031_1920.jpg';
+import productImg9 from '../assets/images/watch-no-numbers.jpg';
+import productImg10 from '../assets/images/watch-with-leather-strap-near-leaves.jpg';
+import productImg11 from '../assets/images/watches-in-black-white.jpg';
+import productImg12 from '../assets/images/wood-leather-watches.jpg';
+import productImg13 from '../assets/images/wrist-watches.jpg';
+
+export const productsDataArray = [
+  {
+    id: 1,
+    name: 'First',
+    price: 1200,
+    image: productImg1,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 1,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 2,
+    name: 'Second',
+    price: 1200,
+    image: productImg2,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 4,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 3,
+    name: 'Third',
+    price: 1400,
+    image: productImg3,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 4,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 4,
+    name: 'Fourth',
+    price: 900,
+    image: productImg4,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 8,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 5,
+    name: 'Fifth',
+    price: 1300,
+    image: productImg5,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 10,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 6,
+    name: 'Sixth',
+    price: 1100,
+    image: productImg6,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 2,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 7,
+    name: 'Seventh',
+    price: 1200,
+    image: productImg7,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 5,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 8,
+    name: 'Eighth',
+    price: 11500,
+    image: productImg8,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 9,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 9,
+    name: 'Ninth',
+    price: 600,
+    image: productImg9,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 1,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 10,
+    name: 'Tenth',
+    price: 1220,
+    image: productImg10,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 4,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 11,
+    name: 'Eleventh',
+    price: 1600,
+    image: productImg11,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 11,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 12,
+    name: 'Twelfth',
+    price: 1990,
+    image: productImg12,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 71,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+  {
+    id: 13,
+    name: 'Thirteenth',
+    price: 1250,
+    image: productImg13,
+    category: 'pocket',
+    // productQuantity: 30,
+    inCartQuantity: 7,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet nesciunt! Maxime error unde corporis laudantium? Nihil, a est! Explicabo?',
+  },
+];
