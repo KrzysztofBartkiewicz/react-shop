142d6f6b39f89f431eedfca8b9abe6ab9217cb4a
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-23 19:05:18 +0200

    make firebade domain transparent

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: 'react-shop-6d00f.firebaseapp.com',
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
